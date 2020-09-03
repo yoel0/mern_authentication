ffa2d87378549fc3e7c419d090ac457dd6ac0f20
yoel <yoel@macbook-pro.home>
2020-09-03 15:08:12 -0700

    [components] add about and footer components

---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>This is all about us.</p>
+			<p>Hipster Ipsum Adidas</p>
+		</div>
+	);
+};
+
+export default About;
