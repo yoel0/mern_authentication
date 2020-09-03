e8290a102c6d8e08f2859c66bdef38bd7f00f934
yoel <yoel@macbook-pro.home>
2020-09-03 16:25:44 -0700

    [App] Add Private Route and Component Logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Profile from './components/Profile';
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+const PrivateRoute = ({ component: Component, ...rest }) => {
+	const user = localStorage.getItem('jwtToken');
 	return (
-		<div className='App'>
-			<Navbar />
-			<Welcome />
+		<Route
+			{...rest}
+			render={props => {
+				return user ? (
+					<Component {...rest} {...props} />
+				) : (
+					<Redirect to='/login' />
+				);
+			}}
+		/>
+	);
+};
+
+function App() {
+	// set state values
+	let [currentUser, setCurrentUser] = useState('');
+	let [isAuthenticated, setIsAuthenticated] = useState(true);
+
+	useEffect(() => {
+		let token;
+		if (!localStorage.getItem('jwtToken')) {
+			setIsAuthenticated(false);
+		} else {
+			token = jwt_decode(localStorage.getItem('jwtToken'));
+			setAuthToken(localStorage.jwtToken);
+			setCurrentUser(token);
+			setIsAuthenticated(true);
+		}
+	}, []);
+
+	const nowCurrentUser = userData => {
+		console.log('nowCurrentUser is working..');
+		setCurrentUser(userData);
+		setIsAuthenticated(true);
+	};
+
+	const handleLogout = () => {
+		if (localStorage.getItem('jwtToken')) {
+			localStorage.removeItem('jwtToken');
+			setCurrentUser(null);
+			setIsAuthenticated(false);
+		}
+	};
+
+	console.log('Current User', currentUser);
+	console.log('Authenticated', isAuthenticated);
+
+	return (
+		<div>
+			<Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
+			<div className='container mt-5'>
+				<Switch>
+					<Route path='/signup' component={Signup} />
+					<Route
+						path='/login'
+						render={props => (
+							<Login
+								{...props}
+								nowCurrentUser={nowCurrentUser}
+								setIsAuthenticated={setIsAuthenticated}
+								user={currentUser}
+							/>
+						)}
+					/>
+					<Route path='/about' cmponent={About} />
+					<PrivateRoute
+						path='/profile'
+						component={Profile}
+						user={currentUser}
+					/>
+					<Route exact path='/' component={Welcome} />
+				</Switch>
+			</div>
 			<Footer />
 		</div>
 	);
